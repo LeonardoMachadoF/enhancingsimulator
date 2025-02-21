d81a3456e4d2bfb8fc6c63cc13a28ccd272ccad0
Leonardo Machado <leonardomachado200112@gmail.com>
2025-02-21 13:13:13 -0300

    hardcoded spent more than

--- Utils/ChannelService.cs
+++ Utils/ChannelService.cs
@@ -3,7 +3,7 @@ namespace Enhance.Utils;
 public static class ChannelService
 {
     public static void ProcessResults((int Fluorite, int BlessedScroll, int Crystal)[] results,
-        double totalExecutionTime)
+        double totalExecutionTime, int spentMoreThan = 2620)
     {
         var totalFluorite = results.Sum(r => r.Fluorite);
         var totalBlessedScroll = results.Sum(r => r.BlessedScroll);
@@ -19,11 +19,14 @@ public static class ChannelService
         var medianBlessedScroll = Calculate.Median(results.Select(r => r.BlessedScroll).ToList());
         var medianCrystal = Calculate.Median(results.Select(r => r.Crystal).ToList());
 
+        var spentMoreFluoriteThan =
+            results.Count(r => r.Fluorite > spentMoreThan);
+
         Console.WriteLine($"Média de recursos consumidos após {simulationsCompleted} simulações:");
         Console.WriteLine($"Fluorite: {averageFluorite:F2} (Mediana: {medianFluorite})");
         Console.WriteLine($"BlessedScroll: {averageBlessedScroll:F2} (Mediana: {medianBlessedScroll})");
         Console.WriteLine($"Crystal: {averageCrystal:F2} (Mediana: {medianCrystal})");
-
+        Console.WriteLine($"{spentMoreFluoriteThan} gastaram mais que {spentMoreThan} fluorites.");
         Console.WriteLine($"Tempo total de execução: {totalExecutionTime:F2} segundos");
     }
 }
